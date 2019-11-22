1617e8cdded9d8212c5b8f4c53ca380c50957a0d
Henry <hgrant94@gmail.com>
2019-11-22 02:04:31 -0500

    indents

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export default () => {
-    return axios.create({
-        baseURL: `http://localhost:8081/`
-    })
+  return axios.create({
+    baseURL: `http://localhost:8081/`
+  })
 }
 
