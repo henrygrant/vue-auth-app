2c0d74a2ae846ba92258cd4e42724136b51293cc
Henry <hgrant94@gmail.com>
2019-12-01 18:19:47 -0500

    move header shit out of app.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,62 +1,7 @@
 <template>
     <div id="app">
       <v-app>
-        <v-app-bar dark short elevate-on-scroll app>
-          <img src="./assets/horde.png" class="logo-img">
-
-          <v-btn
-            text
-            to="/"
-            class="ml-4"
-          >
-            <div>&lt;Bunky Boyz&gt;</div>
-          </v-btn>
-
-          <v-btn
-              text
-              class="ml-2"
-              to="/apply"
-          >
-            Apply
-          </v-btn>
-
-          <v-btn
-              text
-              class="ml-2"
-              to="/admin"
-              v-if="
-              $store.state.isUserLoggedIn &&
-              $store.state.user &&
-              $store.state.user.isAdmin"
-          >
-            Admin
-          </v-btn>
-
-          <v-spacer></v-spacer>
-
-          <v-btn
-              text
-              to="/login"
-              v-if="!$store.state.isUserLoggedIn"
-          >
-            Log In
-          </v-btn>
-          <v-btn
-              text
-              class="ml-2"
-              to="/register"
-              v-if="!$store.state.isUserLoggedIn"
-          >
-            Sign Up
-          </v-btn>
-          <v-btn
-              text
-              v-if="$store.state.isUserLoggedIn"
-              @click="logout"
-          >
-            Log Out
-          </v-btn>
-        </v-app-bar>
+        <page-header></page-header>
         <v-content class="grey darken-2">
           <v-container fluid>
               <router-view></router-view>
@@ -72,17 +17,6 @@
     name: 'app',
     components: {
       PageHeader
-    },
-    methods: {
-      async logout () {
-        await this.$store.dispatch('setToken', null)
-        await this.$store.dispatch('setUser', null)
-        if (this.$router.history.current.name !== 'root') {
-          await this.$router.push({
-            name: 'root'
-          })
-        }
-      }
     }
   }
 </script>
--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,9 +1,75 @@
 <template>
+<v-app-bar dark short elevate-on-scroll app>
+  <img src="./../assets/horde.png" class="logo-img">
 
+  <v-btn
+    text
+    to="/"
+    class="ml-4"
+  >
+    <div>&lt;Bunky Boyz&gt;</div>
+  </v-btn>
+
+  <v-btn
+      text
+      class="ml-2"
+      to="/apply"
+  >
+    Apply
+  </v-btn>
+
+  <v-btn
+      text
+      class="ml-2"
+      to="/admin"
+      v-if="
+      $store.state.isUserLoggedIn &&
+      $store.state.user &&
+      $store.state.user.isAdmin"
+  >
+    Admin
+  </v-btn>
+
+  <v-spacer></v-spacer>
+
+  <v-btn
+      text
+      to="/login"
+      v-if="!$store.state.isUserLoggedIn"
+  >
+    Log In
+  </v-btn>
+  <v-btn
+      text
+      class="ml-2"
+      to="/register"
+      v-if="!$store.state.isUserLoggedIn"
+  >
+    Sign Up
+  </v-btn>
+  <v-btn
+      text
+      v-if="$store.state.isUserLoggedIn"
+      @click="logout"
+  >
+    Log Out
+  </v-btn>
+</v-app-bar>
 </template>
 
 <script>
 export default {
+  methods: {
+    async logout () {
+      await this.$store.dispatch('setToken', null)
+      await this.$store.dispatch('setUser', null)
+      if (this.$router.history.current.name !== 'root') {
+        await this.$router.push({
+          name: 'root'
+        })
+      }
+    }
+  }
 }
 </script>
 
